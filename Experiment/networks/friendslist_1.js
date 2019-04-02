412e3a44bae9d79a96fabb211e404aacc01ec29a
Vincent Wong <vmwong1@gmail.com>
2019-04-02 01:27:18 -0400

    Made changes to add mock platform to website

---
+++ Experiment/networks/friendslist_1.js
@@ -0,0 +1,820 @@
+var friendlist = {
+    "2toneKing_Goat": [
+        "Vicario_Goat",
+        "StrictBusiness_Cow-1"
+    ],
+    "45Divide_Unicorn": [
+        "MrShroud_Unicorn",
+        "MeltyBats_Unicorn",
+        "StrikerZero_Unicorn"
+    ],
+    "4Dfloor_Unicorn": [
+        "MrShroud_Unicorn",
+        "OPTIC_Bear"
+    ],
+    "5DollarPool_Cow-1": [
+        "WubWub_Goat",
+        "WanderLost_Goose",
+        "N0R4_Lion",
+        "MoreReeds_Goat",
+        "Turtinator_Cow-1",
+        "TheMacabre_Goat"
+    ],
+    "5TineFork_Lion": [
+        "ChildOfFire_Lion",
+        "N0R4_Lion",
+        "Bashtion_Lion",
+        "TunnelOfFire_Cow-1",
+        "Rob1096_Lion",
+        "SoulJan_Lion",
+        "TheMacabre_Goat",
+        "Turtinator_Cow-1",
+        "Rickr6_Bear",
+        "Fred2Day_Cat"
+    ],
+    "Alexazam_Cat": [
+        "Fly590_Raven"
+    ],
+    "AppleCurryPie_Cat": [
+        "HackerClue_Cat",
+        "Glimpse294_Lion",
+        "Fred2Day_Cat"
+    ],
+    "Aura_Cat": [
+        "Robcat_Cat",
+        "SkeletonFan_Cat",
+        "TrumpetHeart_Goat",
+        "Fly590_Raven",
+        "Fred2Day_Cat"
+    ],
+    "AxtonRose_Raven": [
+        "SunderBar_Goose",
+        "Newtork_Goose",
+        "ClockStar_Raven"
+    ],
+    "BL0CKTR4P_Raven": [
+        "Bernardx_Unicorn",
+        "xValor_Cow-1"
+    ],
+    "Bashtion_Lion": [
+        "Skyfox_Unicorn",
+        "MeltyBats_Unicorn",
+        "FlyStone_Bear",
+        "Legalend_Bear",
+        "TeaTreatTime_Bear",
+        "DesertSky_Bear",
+        "MoreReeds_Goat",
+        "Rickr6_Bear",
+        "xValor_Cow-1",
+        "Fred2Day_Cat",
+        "deValencia_Cat",
+        "RegnumAlya_Bear",
+        "Robcat_Cat",
+        "TheMacabre_Goat",
+        "ChildOfFire_Lion",
+        "JamFan56_Bear",
+        "TwinCoastBoat_Cat",
+        "FromAshes_Bear",
+        "Rob1096_Lion",
+        "BlanketHeart_Raven",
+        "Turtinator_Cow-1",
+        "OPTIC_Bear",
+        "5TineFork_Lion"
+    ],
+    "BeatnikDitty_Goose": [
+        "SunderBar_Goose",
+        "Robcat_Cat"
+    ],
+    "Bernardx_Unicorn": [
+        "BL0CKTR4P_Raven",
+        "WanderLost_Goose",
+        "xValor_Cow-1",
+        "Inchworm_Raven",
+        "Philanthropos_Raven",
+        "EmberFury_Raven"
+    ],
+    "BlanketHeart_Raven": [
+        "RegnumAlya_Bear",
+        "JamFan56_Bear",
+        "Bashtion_Lion"
+    ],
+    "BoredForge_Goat": [
+        "CookieBookie_Goat",
+        "Robcat_Cat"
+    ],
+    "BurnPuppy_Goat": [
+        "Vicario_Goat",
+        "MoreReeds_Goat"
+    ],
+    "C4irn_Raven": [
+        "Dimple5_Raven",
+        "PebbleCandy_Raven"
+    ],
+    "ChildOfFire_Lion": [
+        "DesertSky_Bear",
+        "LinkBob_Goat",
+        "Nocturnez_Lion",
+        "Fred2Day_Cat",
+        "Bashtion_Lion",
+        "Rob1096_Lion",
+        "SunsetZephyr_Lion",
+        "5TineFork_Lion"
+    ],
+    "ClockStar_Raven": [
+        "Newtork_Goose",
+        "SunderBar_Goose",
+        "AxtonRose_Raven"
+    ],
+    "Coffee987_Cow-1": [
+        "Skyfox_Unicorn",
+        "TunnelOfFire_Cow-1",
+        "MoreReeds_Goat",
+        "Rob1096_Lion",
+        "TiredChimes_Cow-1",
+        "Memoralis8_Cat",
+        "Turtinator_Cow-1",
+        "FromAshes_Bear"
+    ],
+    "CookieBookie_Goat": [
+        "WubWub_Goat",
+        "Robcat_Cat",
+        "JamFan56_Bear",
+        "BoredForge_Goat",
+        "MoreReeds_Goat"
+    ],
+    "CreekJump_Goat": [
+        "Skyfox_Unicorn",
+        "Nocturnez_Lion",
+        "Robcat_Cat",
+        "MilitaryHair_Goat"
+    ],
+    "DesertSky_Bear": [
+        "Skyfox_Unicorn",
+        "ChildOfFire_Lion",
+        "N0R4_Lion",
+        "Bashtion_Lion",
+        "Legalend_Bear",
+        "OPTIC_Bear"
+    ],
+    "Dimple5_Raven": [
+        "xValor_Cow-1",
+        "GlinkaFan10_Raven",
+        "PebbleCandy_Raven",
+        "Fred2Day_Cat",
+        "TrumpetHeart_Goat",
+        "MotherBoar_Raven",
+        "TheVeryCore_Raven",
+        "C4irn_Raven",
+        "TwinCoastBoat_Cat",
+        "MoreReeds_Goat",
+        "Rob1096_Lion",
+        "TheMacabre_Goat",
+        "Fly590_Raven"
+    ],
+    "EmberFury_Raven": [
+        "SimonBrink_Raven",
+        "Bernardx_Unicorn",
+        "xValor_Cow-1"
+    ],
+    "EpicPrincess_Bear": [
+        "SquadjobGob_Bear",
+        "WestonCreation_Bear",
+        "EpicPrincess_Bear"
+    ],
+    "FaradayFire_Lion": [
+        "RizuKing_Lion"
+    ],
+    "Flick_Lion": [
+        "N0R4_Lion"
+    ],
+    "Fly590_Raven": [
+        "WanderLost_Goose",
+        "xValor_Cow-1",
+        "GlinkaFan10_Raven",
+        "Alexazam_Cat",
+        "Aura_Cat",
+        "Philanthropos_Raven",
+        "Dimple5_Raven",
+        "Robcat_Cat",
+        "Rickr6_Bear",
+        "TheMacabre_Goat"
+    ],
+    "FlyStone_Bear": [
+        "FromAshes_Bear",
+        "MeltyBats_Unicorn",
+        "Bashtion_Lion",
+        "ShadowX_Unicorn",
+        "TeaTreatTime_Bear",
+        "TheMacabre_Goat"
+    ],
+    "Fred2Day_Cat": [
+        "HackerClue_Cat",
+        "WanderLost_Goose",
+        "xValor_Cow-1",
+        "Bashtion_Lion",
+        "Aura_Cat",
+        "Dimple5_Raven",
+        "TrumpetHeart_Goat",
+        "Robcat_Cat",
+        "AppleCurryPie_Cat",
+        "SavageDork_Cat",
+        "TheMacabre_Goat",
+        "FromAshes_Bear",
+        "ChildOfFire_Lion",
+        "Limerick_Cat",
+        "TwinCoastBoat_Cat",
+        "Glimpse294_Lion",
+        "Rob1096_Lion",
+        "5TineFork_Lion",
+        "Turtinator_Cow-1",
+        "Rickr6_Bear",
+        "N0R4_Lion"
+    ],
+    "FromAshes_Bear": [
+        "Skyfox_Unicorn",
+        "Memoralis8_Cat",
+        "FlyStone_Bear",
+        "Fred2Day_Cat",
+        "Bashtion_Lion",
+        "Limerick_Cat",
+        "TrumpetHeart_Goat",
+        "TheMacabre_Goat",
+        "JamFan56_Bear",
+        "MoreReeds_Goat",
+        "Coffee987_Cow-1",
+        "Turtinator_Cow-1",
+        "Rickr6_Bear"
+    ],
+    "GeistBust_Unicorn": [
+        "MeltyBats_Unicorn",
+        "OPTIC_Bear",
+        "StriveDiver_Bear"
+    ],
+    "GlareBear_Goat": [
+        "Skyfox_Unicorn",
+        "WubWub_Goat"
+    ],
+    "Glimpse294_Lion": [
+        "AppleCurryPie_Cat",
+        "Fred2Day_Cat"
+    ],
+    "GlinkaFan10_Raven": [
+        "Dimple5_Raven",
+        "MotherBoar_Raven",
+        "Fly590_Raven",
+        "NumberMuncher_Raven",
+        "TheVeryCore_Raven"
+    ],
+    "HackerClue_Cat": [
+        "Fred2Day_Cat",
+        "SavageDork_Cat",
+        "AppleCurryPie_Cat"
+    ],
+    "Inchworm_Raven": [
+        "Bernardx_Unicorn",
+        "TwinCoastBoat_Cat",
+        "xValor_Cow-1"
+    ],
+    "IpsoFasto_Lion": [
+        "SoundWolf_Lion"
+    ],
+    "JamFan56_Bear": [
+        "CookieBookie_Goat",
+        "deValencia_Cat",
+        "Bashtion_Lion",
+        "TempleOfTheDogs_Bear",
+        "RegnumAlya_Bear",
+        "Legalend_Bear",
+        "TheMacabre_Goat",
+        "Robcat_Cat",
+        "Rickr6_Bear",
+        "BlanketHeart_Raven",
+        "WestonCreation_Bear",
+        "FromAshes_Bear"
+    ],
+    "Legalend_Bear": [
+        "DesertSky_Bear",
+        "JamFan56_Bear",
+        "Bashtion_Lion"
+    ],
+    "Limerick_Cat": [
+        "Memoralis8_Cat",
+        "TwinCoastBoat_Cat",
+        "Fred2Day_Cat",
+        "Turtinator_Cow-1",
+        "FromAshes_Bear",
+        "SavageDork_Cat"
+    ],
+    "LinkBob_Goat": [
+        "ChildOfFire_Lion",
+        "Nocturnez_Lion",
+        "SunsetZephyr_Lion"
+    ],
+    "ManiaDaze_Lion": [
+        "RizuKing_Lion",
+        "SoundWolf_Lion",
+        "N0R4_Lion"
+    ],
+    "MeltyBats_Unicorn": [
+        "Skyfox_Unicorn",
+        "StriveDiver_Bear",
+        "FlyStone_Bear",
+        "GeistBust_Unicorn",
+        "N0R4_Lion",
+        "Bashtion_Lion",
+        "MrShroud_Unicorn",
+        "45Divide_Unicorn",
+        "ShadowX_Unicorn",
+        "MoreReeds_Goat",
+        "Turtinator_Cow-1",
+        "StrikerZero_Unicorn",
+        "OPTIC_Bear"
+    ],
+    "Memoralis8_Cat": [
+        "Coffee987_Cow-1",
+        "FromAshes_Bear",
+        "Limerick_Cat"
+    ],
+    "MilitaryHair_Goat": [
+        "Skyfox_Unicorn",
+        "TrumpetHeart_Goat",
+        "CreekJump_Goat"
+    ],
+    "MoreReeds_Goat": [
+        "Skyfox_Unicorn",
+        "WubWub_Goat",
+        "xValor_Cow-1",
+        "CookieBookie_Goat",
+        "N0R4_Lion",
+        "Bashtion_Lion",
+        "MeltyBats_Unicorn",
+        "Dimple5_Raven",
+        "Robcat_Cat",
+        "Nocturnez_Lion",
+        "Vicario_Goat",
+        "FromAshes_Bear",
+        "BurnPuppy_Goat",
+        "Rob1096_Lion",
+        "5DollarPool_Cow-1",
+        "Coffee987_Cow-1",
+        "Rickr6_Bear",
+        "TheMacabre_Goat",
+        "Turtinator_Cow-1",
+        "OPTIC_Bear"
+    ],
+    "MotherBoar_Raven": [
+        "Dimple5_Raven",
+        "GlinkaFan10_Raven"
+    ],
+    "MrShroud_Unicorn": [
+        "MeltyBats_Unicorn",
+        "45Divide_Unicorn",
+        "OPTIC_Bear",
+        "4Dfloor_Unicorn"
+    ],
+    "N0R4_Lion": [
+        "Skyfox_Unicorn",
+        "MeltyBats_Unicorn",
+        "Nocturnez_Lion",
+        "Fred2Day_Cat",
+        "Xalentor_Goat",
+        "RizuKing_Lion",
+        "ManiaDaze_Lion",
+        "Robcat_Cat",
+        "5DollarPool_Cow-1",
+        "TheMacabre_Goat",
+        "DesertSky_Bear",
+        "Flick_Lion",
+        "MoreReeds_Goat",
+        "Rob1096_Lion",
+        "Turtinator_Cow-1",
+        "OPTIC_Bear",
+        "5TineFork_Lion"
+    ],
+    "NewNerd_Cow-1": [
+        "SatyrGate_Cat",
+        "v2Karate_Cat",
+        "Robcat_Cat"
+    ],
+    "Newtork_Goose": [
+        "Robcat_Cat",
+        "SunderBar_Goose",
+        "AxtonRose_Raven",
+        "ClockStar_Raven",
+        "TheMacabre_Goat"
+    ],
+    "Nocturnez_Lion": [
+        "Skyfox_Unicorn",
+        "ChildOfFire_Lion",
+        "CreekJump_Goat",
+        "TwinCoastBoat_Cat",
+        "N0R4_Lion",
+        "MoreReeds_Goat",
+        "LinkBob_Goat",
+        "Rob1096_Lion",
+        "PinkInstant_Lion",
+        "RizuKing_Lion"
+    ],
+    "NumberMuncher_Raven": [
+        "GlinkaFan10_Raven",
+        "NumberMuncher_Raven"
+    ],
+    "OPTIC_Bear": [
+        "Skyfox_Unicorn",
+        "MeltyBats_Unicorn",
+        "GeistBust_Unicorn",
+        "N0R4_Lion",
+        "Bashtion_Lion",
+        "MrShroud_Unicorn",
+        "4Dfloor_Unicorn",
+        "TheMacabre_Goat",
+        "DesertSky_Bear",
+        "ShadowX_Unicorn",
+        "TwinCoastBoat_Cat",
+        "MoreReeds_Goat",
+        "Turtinator_Cow-1"
+    ],
+    "PebbleCandy_Raven": [
+        "Dimple5_Raven",
+        "C4irn_Raven"
+    ],
+    "Philanthropos_Raven": [
+        "xValor_Cow-1",
+        "Bernardx_Unicorn",
+        "Fly590_Raven",
+        "Rickr6_Bear",
+        "Spike80_Raven"
+    ],
+    "PinkInstant_Lion": [
+        "RizuKing_Lion",
+        "Nocturnez_Lion"
+    ],
+    "PrismaticTwist_Unicorn": [
+        "PrismaticTwist_Unicorn",
+        "StrikerZero_Unicorn"
+    ],
+    "RegnumAlya_Bear": [
+        "TempleOfTheDogs_Bear",
+        "BlanketHeart_Raven",
+        "JamFan56_Bear",
+        "Bashtion_Lion",
+        "Spenderson_Cat"
+    ],
+    "Rickr6_Bear": [
+        "Skyfox_Unicorn",
+        "xValor_Cow-1",
+        "Fred2Day_Cat",
+        "deValencia_Cat",
+        "Philanthropos_Raven",
+        "v2Karate_Cat",
+        "Robcat_Cat",
+        "SavageDork_Cat",
+        "Bashtion_Lion",
+        "TunnelOfFire_Cow-1",
+        "JamFan56_Bear",
+        "TwinCoastBoat_Cat",
+        "MoreReeds_Goat",
+        "Rob1096_Lion",
+        "Fly590_Raven",
+        "Turtinator_Cow-1",
+        "FromAshes_Bear",
+        "5TineFork_Lion"
+    ],
+    "RizuKing_Lion": [
+        "FaradayFire_Lion",
+        "PinkInstant_Lion",
+        "Nocturnez_Lion",
+        "ManiaDaze_Lion",
+        "N0R4_Lion"
+    ],
+    "Rob1096_Lion": [
+        "Skyfox_Unicorn",
+        "Nocturnez_Lion",
+        "Fred2Day_Cat",
+        "Bashtion_Lion",
+        "Dimple5_Raven",
+        "SoulJan_Lion",
+        "Robcat_Cat",
+        "xValor_Cow-1",
+        "Vicario_Goat",
+        "ChildOfFire_Lion",
+        "TwinCoastBoat_Cat",
+        "MoreReeds_Goat",
+        "Coffee987_Cow-1",
+        "5TineFork_Lion",
+        "Turtinator_Cow-1",
+        "Rickr6_Bear",
+        "N0R4_Lion"
+    ],
+    "Robcat_Cat": [
+        "Skyfox_Unicorn",
+        "CookieBookie_Goat",
+        "MoreReeds_Goat",
+        "v2Karate_Cat",
+        "SunderBar_Goose",
+        "Rob1096_Lion",
+        "Aura_Cat",
+        "NewNerd_Cow-1",
+        "Rickr6_Bear",
+        "deValencia_Cat",
+        "N0R4_Lion",
+        "SkeletonFan_Cat",
+        "xValor_Cow-1",
+        "CreekJump_Goat",
+        "BoredForge_Goat",
+        "Fred2Day_Cat",
+        "Bashtion_Lion",
+        "Turtinator_Cow-1",
+        "SavageDork_Cat",
+        "TheMacabre_Goat",
+        "TunnelOfFire_Cow-1",
+        "BeatnikDitty_Goose",
+        "Newtork_Goose",
+        "JamFan56_Bear",
+        "SatyrGate_Cat",
+        "Fly590_Raven"
+    ],
+    "RoiVert_Goat": [
+        "Vicario_Goat",
+        "xGlory_Goat"
+    ],
+    "SatyrGate_Cat": [
+        "Robcat_Cat",
+        "NewNerd_Cow-1"
+    ],
+    "SavageDork_Cat": [
+        "HackerClue_Cat",
+        "Turtinator_Cow-1",
+        "Fred2Day_Cat",
+        "Limerick_Cat",
+        "v2Karate_Cat",
+        "Robcat_Cat",
+        "Rickr6_Bear"
+    ],
+    "ShadowX_Unicorn": [
+        "MeltyBats_Unicorn",
+        "FlyStone_Bear",
+        "OPTIC_Bear"
+    ],
+    "Shell12_Cat": [
+        "TwinCoastBoat_Cat"
+    ],
+    "SimonBrink_Raven": [
+        "EmberFury_Raven"
+    ],
+    "SkeletonFan_Cat": [
+        "Robcat_Cat",
+        "Aura_Cat"
+    ],
+    "Skyfox_Unicorn": [
+        "WubWub_Goat",
+        "MeltyBats_Unicorn",
+        "Nocturnez_Lion",
+        "CreekJump_Goat",
+        "N0R4_Lion",
+        "Bashtion_Lion",
+        "TrumpetHeart_Goat",
+        "Robcat_Cat",
+        "MilitaryHair_Goat",
+        "Rickr6_Bear",
+        "DesertSky_Bear",
+        "Coffee987_Cow-1",
+        "FromAshes_Bear",
+        "MoreReeds_Goat",
+        "Rob1096_Lion",
+        "Zoe53_Bear",
+        "OPTIC_Bear",
+        "GlareBear_Goat"
+    ],
+    "SoulJan_Lion": [
+        "Rob1096_Lion",
+        "5TineFork_Lion"
+    ],
+    "SoundWolf_Lion": [
+        "ManiaDaze_Lion",
+        "IpsoFasto_Lion"
+    ],
+    "Spenderson_Cat": [
+        "RegnumAlya_Bear"
+    ],
+    "Spike80_Raven": [
+        "Philanthropos_Raven"
+    ],
+    "SquadjobGob_Bear": [
+        "WestonCreation_Bear",
+        "EpicPrincess_Bear"
+    ],
+    "StrictBusiness_Cow-1": [
+        "Vicario_Goat",
+        "2toneKing_Goat"
+    ],
+    "StrikerZero_Unicorn": [
+        "MeltyBats_Unicorn",
+        "PrismaticTwist_Unicorn",
+        "45Divide_Unicorn"
+    ],
+    "StriveDiver_Bear": [
+        "TwrlGrl_Unicorn",
+        "MeltyBats_Unicorn",
+        "GeistBust_Unicorn"
+    ],
+    "SunderBar_Goose": [
+        "BeatnikDitty_Goose",
+        "Newtork_Goose",
+        "ClockStar_Raven",
+        "AxtonRose_Raven",
+        "Robcat_Cat",
+        "TheMacabre_Goat"
+    ],
+    "SunsetZephyr_Lion": [
+        "LinkBob_Goat",
+        "ChildOfFire_Lion"
+    ],
+    "TeaTreatTime_Bear": [
+        "FlyStone_Bear",
+        "Bashtion_Lion"
+    ],
+    "TempleOfTheDogs_Bear": [
+        "RegnumAlya_Bear",
+        "JamFan56_Bear"
+    ],
+    "TheImmolator_Goose": [
+        "TunnelOfFire_Cow-1",
+        "TheMacabre_Goat"
+    ],
+    "TheMacabre_Goat": [
+        "FlyStone_Bear",
+        "SunderBar_Goose",
+        "Bashtion_Lion",
+        "TheImmolator_Goose",
+        "Turtinator_Cow-1",
+        "MoreReeds_Goat",
+        "Fly590_Raven",
+        "FromAshes_Bear",
+        "N0R4_Lion",
+        "xValor_Cow-1",
+        "Fred2Day_Cat",
+        "deValencia_Cat",
+        "Dimple5_Raven",
+        "Robcat_Cat",
+        "5DollarPool_Cow-1",
+        "TunnelOfFire_Cow-1",
+        "JamFan56_Bear",
+        "TwinCoastBoat_Cat",
+        "Newtork_Goose",
+        "WestonCreation_Bear",
+        "OPTIC_Bear",
+        "5TineFork_Lion"
+    ],
+    "TheVeryCore_Raven": [
+        "Dimple5_Raven",
+        "GlinkaFan10_Raven"
+    ],
+    "TiredChimes_Cow-1": [
+        "Coffee987_Cow-1",
+        "TunnelOfFire_Cow-1"
+    ],
+    "TrumpetHeart_Goat": [
+        "Skyfox_Unicorn",
+        "Fred2Day_Cat",
+        "Aura_Cat",
+        "Dimple5_Raven",
+        "Varelia_Goat",
+        "FromAshes_Bear",
+        "MilitaryHair_Goat",
+        "Zoe53_Bear"
+    ],
+    "TunnelOfFire_Cow-1": [
+        "TheImmolator_Goose",
+        "Coffee987_Cow-1",
+        "Turtinator_Cow-1",
+        "TiredChimes_Cow-1",
+        "TheMacabre_Goat",
+        "Robcat_Cat",
+        "Rickr6_Bear",
+        "5TineFork_Lion"
+    ],
+    "Turtinator_Cow-1": [
+        "WubWub_Goat",
+        "WanderLost_Goose",
+        "xValor_Cow-1",
+        "Fred2Day_Cat",
+        "Bashtion_Lion",
+        "MeltyBats_Unicorn",
+        "Limerick_Cat",
+        "Robcat_Cat",
+        "Rob1096_Lion",
+        "5DollarPool_Cow-1",
+        "TheMacabre_Goat",
+        "FromAshes_Bear",
+        "TunnelOfFire_Cow-1",
+        "SavageDork_Cat",
+        "MoreReeds_Goat",
+        "N0R4_Lion",
+        "Coffee987_Cow-1",
+        "Rickr6_Bear",
+        "OPTIC_Bear",
+        "5TineFork_Lion"
+    ],
+    "TwinCoastBoat_Cat": [
+        "xValor_Cow-1",
+        "Fred2Day_Cat",
+        "deValencia_Cat",
+        "Shell12_Cat",
+        "Limerick_Cat",
+        "OPTIC_Bear",
+        "Nocturnez_Lion",
+        "TheMacabre_Goat",
+        "Inchworm_Raven",
+        "Dimple5_Raven",
+        "Rob1096_Lion",
+        "Rickr6_Bear",
+        "Bashtion_Lion"
+    ],
+    "TwrlGrl_Unicorn": [
+        "StriveDiver_Bear"
+    ],
+    "Varelia_Goat": [
+        "TrumpetHeart_Goat"
+    ],
+    "Vicario_Goat": [
+        "MoreReeds_Goat",
+        "BurnPuppy_Goat",
+        "2toneKing_Goat",
+        "Rob1096_Lion",
+        "RoiVert_Goat",
+        "StrictBusiness_Cow-1",
+        "xGlory_Goat"
+    ],
+    "WanderLost_Goose": [
+        "xValor_Cow-1",
+        "Fred2Day_Cat",
+        "Turtinator_Cow-1",
+        "Bernardx_Unicorn",
+        "Fly590_Raven",
+        "5DollarPool_Cow-1"
+    ],
+    "WestonCreation_Bear": [
+        "SquadjobGob_Bear",
+        "JamFan56_Bear",
+        "EpicPrincess_Bear",
+        "TheMacabre_Goat"
+    ],
+    "WubWub_Goat": [
+        "Skyfox_Unicorn",
+        "CookieBookie_Goat",
+        "MoreReeds_Goat",
+        "GlareBear_Goat",
+        "Turtinator_Cow-1",
+        "5DollarPool_Cow-1"
+    ],
+    "Xalentor_Goat": [
+        "N0R4_Lion"
+    ],
+    "Zoe53_Bear": [
+        "Skyfox_Unicorn",
+        "TrumpetHeart_Goat"
+    ],
+    "deValencia_Cat": [
+        "xValor_Cow-1",
+        "JamFan56_Bear",
+        "TwinCoastBoat_Cat",
+        "Bashtion_Lion",
+        "v2Karate_Cat",
+        "Robcat_Cat",
+        "Rickr6_Bear",
+        "TheMacabre_Goat"
+    ],
+    "v2Karate_Cat": [
+        "deValencia_Cat",
+        "Robcat_Cat",
+        "SavageDork_Cat",
+        "NewNerd_Cow-1",
+        "Rickr6_Bear"
+    ],
+    "xGlory_Goat": [
+        "RoiVert_Goat",
+        "Vicario_Goat"
+    ],
+    "xValor_Cow-1": [
+        "WanderLost_Goose",
+        "xValor_Cow-1",
+        "Fred2Day_Cat",
+        "Bashtion_Lion",
+        "Philanthropos_Raven",
+        "Dimple5_Raven",
+        "Bernardx_Unicorn",
+        "MoreReeds_Goat",
+        "Robcat_Cat",
+        "TheMacabre_Goat",
+        "BL0CKTR4P_Raven",
+        "Rob1096_Lion",
+        "TwinCoastBoat_Cat",
+        "Inchworm_Raven",
+        "EmberFury_Raven",
+        "Rickr6_Bear",
+        "Fly590_Raven",
+        "Turtinator_Cow-1",
+        "deValencia_Cat"
+    ]
+}
